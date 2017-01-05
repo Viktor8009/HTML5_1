35018215c25446b0ed9b839823a618c9701c34ce
Viktor8009 <csangov@gmail.com>
2017-01-05 21:33:34 +0100

    HTML5 elements and registration

---
+++ js/main.js
@@ -0,0 +1,15 @@
+function showRegistration(){
+    
+    var regShow=document.querySelector("#section2");
+    
+    regShow.classList.add("big");
+}
+
+function hideRegistration(){
+    
+    var regHide = document.querySelector("#section2");
+    
+    regHide.classList.remove("big");
+    
+    
+}
